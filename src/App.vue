9ddbce177371f3d9fa7b5c9c99351d6e7a2b4723
chapkovski <chapkovski@gmail.com>
2020-11-25 22:36:22 +0300

    upd menu

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,69 @@
 <template>
-    <v-app>
-        <v-app-bar color="#6A76AB" dark app height='95' >
-    
-            <v-app-bar-nav-icon></v-app-bar-nav-icon>
-    
-           
-      <account-info>
-    </account-info>
-            <v-spacer></v-spacer>
-            <div class="m-3">
-                <v-tooltip bottom>
-                    <template v-slot:activator="{ on, attrs }">
-                <div v-bind="attrs" v-on="on">
-                  <v-badge avatar bordered overlap color="error">
-                    <template v-slot:badge>
-                      <v-avatar>
-                        2
-                      </v-avatar>
-</template>
+  <v-app>
+    <v-app-bar color="#6A76AB" dark app height="95">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <account-info> </account-info>
+      <v-spacer></v-spacer>
+      <div :class="{  'd-flex':true}"  v-if='inTrade'>
+        <div class="m-3">
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on, attrs }">
+              <div v-bind="attrs" v-on="on">
+                <v-badge avatar bordered overlap color="error">
+                  <template v-slot:badge>
+                    <v-avatar>
+                      2
+                    </v-avatar>
+                  </template>
+
+                  <v-avatar size="60">
+                    <v-img
+                      src="https://cdn1.iconfinder.com/data/icons/web-design-18/50/86-512.png"
+                    ></v-img>
+                  </v-avatar>
+                </v-badge>
+              </div>
+            </template>
+            <span>Amount of trade > 1000</span>
+          </v-tooltip>
+        </div>
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <div v-bind="attrs" v-on="on">
+              <v-badge avatar bordered overlap>
+                <template v-slot:badge>
+                  <v-avatar>
+                    2
+                  </v-avatar>
+                </template>
 
                 <v-avatar size="60">
                   <v-img
-                    src="https://cdn1.iconfinder.com/data/icons/web-design-18/50/86-512.png"
+                    src="https://previews.123rf.com/images/pikepicture/pikepicture1807/pikepicture180700221/104970298-best-worker-employee-vector-european-woman-award-of-the-year-gold-wreath-leader-business-cartoon-ill.jpg"
                   ></v-img>
                 </v-avatar>
               </v-badge>
             </div>
           </template>
-          <span>Amount of trade > 1000</span>
+          <span>Best trader of the day</span>
         </v-tooltip>
       </div>
-      <v-tooltip bottom>
-<template v-slot:activator="{ on, attrs }">
-    <div v-bind="attrs" v-on="on">
-        <v-badge avatar bordered overlap>
-            <template v-slot:badge>
-                    <v-avatar>
-                      2
-                    </v-avatar>
-</template>
-
-              <v-avatar size="60">
-                <v-img
-                  src="https://previews.123rf.com/images/pikepicture/pikepicture1807/pikepicture180700221/104970298-best-worker-employee-vector-european-woman-award-of-the-year-gold-wreath-leader-business-cartoon-ill.jpg"
-                ></v-img>
-              </v-avatar>
-            </v-badge>
-          </div>
-        </template>
-        <span>Best trader of the day</span>
-      </v-tooltip>
-    
-
     </v-app-bar>
 
-      
- 
-    <v-navigation-drawer permanent
-        expand-on-hover app>
-<template v-slot:prepend>
-    <v-list-item two-line>
-        <v-list-item-avatar>
+    <v-navigation-drawer permanent expand-on-hover app>
+      <template v-slot:prepend>
+        <v-list-item two-line>
+          <v-list-item-avatar>
             <img src="https://randomuser.me/api/portraits/women/81.jpg" />
-        </v-list-item-avatar>
-    
-        <v-list-item-content>
+          </v-list-item-avatar>
+
+          <v-list-item-content>
             <v-list-item-title>Jane Smith</v-list-item-title>
             <v-list-item-subtitle>Logged In</v-list-item-subtitle>
-        </v-list-item-content>
-    </v-list-item>
-</template>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
 
       <v-divider></v-divider>
 
@@ -91,7 +85,7 @@
       </v-container>
     </v-main>
 
-    <v-footer app height='50'>
+    <v-footer app height="50">
       <trade-footer v-if="inTrade" />
     </v-footer>
   </v-app>
@@ -101,40 +95,40 @@
 import TradeFooter from "trade/TradeFooter";
 import AccountInfo from "bank/AccountInfo";
 export default {
-    components: { TradeFooter, AccountInfo },
-    data() {
-        return {
-            items: [
-                { title: "Trading", icon: "mdi-bank", to: { name: "Trade" } },
-                { title: "Work", icon: "mdi-account-hard-hat", to: { name: "Work" } },
-            ],
-        };
+  components: { TradeFooter, AccountInfo },
+  data() {
+    return {
+      items: [
+        { title: "Trading", icon: "mdi-bank", to: { name: "Trade" } },
+        { title: "Work", icon: "mdi-account-hard-hat", to: { name: "Work" } },
+      ],
+    };
+  },
+  computed: {
+    inTrade() {
+      return this.$route.name == "Trade";
     },
-    computed: {
-        inTrade() {
-            return this.$route.name == "Trade";
-        },
-    },
+  },
 };
 </script>
 
 <style lang="scss">
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
 #nav {
-    padding: 30px;
-    a {
-        font-weight: bold;
-        color: #2c3e50;
-        &.router-link-exact-active {
-            color: #42b983;
-        }
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
     }
+  }
 }
 </style>
--- src/components/bank/AccountInfo.vue
+++ src/components/bank/AccountInfo.vue
@@ -1,62 +1,48 @@
 
 <template>
- 
-   
-      <v-card
-        
-        
-        raised
-        
-       
-        
-        
-       
-      >
+    <v-card raised>
         <v-card-text>
-          Bank account balance:
-            <v-chip
-      class="ma-2"
-      color="primary"
-    >
-      $100.000
-    </v-chip>
+            Bank account balance:
+            <v-chip class="ma-2" color="primary">
+                $100.000
+            </v-chip>
         </v-card-text>
-
-       
-      </v-card>
-
+    
+    
+    </v-card>
 </template>
+
 <script>
-  export default {
+export default {
     data: () => ({
-      icons: [
-        'mdi-home',
-        'mdi-email',
-        'mdi-calendar',
-        'mdi-delete',
-      ],
-      items: [
-        'default',
-        'absolute',
-        'fixed',
-      ],
-      padless: false,
-      variant: 'default',
+        icons: [
+            'mdi-home',
+            'mdi-email',
+            'mdi-calendar',
+            'mdi-delete',
+        ],
+        items: [
+            'default',
+            'absolute',
+            'fixed',
+        ],
+        padless: false,
+        variant: 'default',
     }),
     computed: {
-      localAttrs () {
-        const attrs = {}
+        localAttrs() {
+            const attrs = {}
 
-        if (this.variant === 'default') {
-          attrs.absolute = false
-          attrs.fixed = false
-        } else {
-          attrs[this.variant] = true
-        }
-        return attrs
-      },
+            if (this.variant === 'default') {
+                attrs.absolute = false
+                attrs.fixed = false
+            } else {
+                attrs[this.variant] = true
+            }
+            return attrs
+        },
     },
-  }
+}
 </script>
 
 <style scoped>
