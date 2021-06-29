be3e284a9314196e4704bb297e04c731c9f9de99
Francisco Kahil <codexico@gmail.com>
2021-06-29 22:25:31 +0300

    upd_config_js

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,18 @@
 const BundleTracker = require('webpack-bundle-tracker')
 const path = require('path');
 // Change this to match the path to your files in production (could be S3, CloudFront, etc.)
-const DEPLOYMENT_PATH = '../_static/dist/'
+const DEPLOYMENT_PATH = '/static/dist'
 
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ? DEPLOYMENT_PATH : 'http://localhost:8080/',
     outputDir: '../_static/dist',
 
-    devServer: {
-        public: 'localhost:8080',
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-        },
-    },
+    // devServer: {
+    //     public: 'localhost:8080',
+    //     headers: {
+    //         'Access-Control-Allow-Origin': '*',
+    //     },
+    // },
 
     configureWebpack: {
         
@@ -30,5 +30,17 @@ module.exports = {
             .set('bank', path.resolve(__dirname, 'src/components/bank'))
             .set('trade', path.resolve(__dirname, 'src/components/trade'))
             .set('work', path.resolve(__dirname, 'src/components/work'));
+          config.resolve.alias
+            .set('__STATIC__', 'static');
+
+        config.devServer
+            .public('http://localhost:8080')
+            .host('localhost')
+            .port(8080)
+            .hotOnly(true)
+            .watchOptions({ poll: 1000 })
+            .https(false)
+            .headers({ "Access-Control-Allow-Origin": ["*"] });
+        config.module.rules.delete('eslint');
     },
 }
