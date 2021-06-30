e06e76fffcc304033f4efccf8b899104a750950d
Francisco Kahil <codexico@gmail.com>
2021-06-30 14:16:36 +0300

    upd webpackstats

--- vue.config.js
+++ vue.config.js
@@ -4,21 +4,19 @@ const path = require('path');
 const DEPLOYMENT_PATH = '/static/dist'
 
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? DEPLOYMENT_PATH : 'http://localhost:8080/',
-    outputDir: '../_static/dist',
+        filenameHashing: false,
+    productionSourceMap: false,
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/static/vue'
+        : 'http://localhost:8080/',
+    outputDir: '../_static/vue/',
 
-    // devServer: {
-    //     public: 'localhost:8080',
-    //     headers: {
-    //         'Access-Control-Allow-Origin': '*',
-    //     },
-    // },
 
     configureWebpack: {
-        
+
         plugins: [
             new BundleTracker({path: __dirname, filename: 'webpack-stats.json'}),
-        
+
         ],
     },
     "transpileDependencies": [
@@ -30,7 +28,7 @@ module.exports = {
             .set('bank', path.resolve(__dirname, 'src/components/bank'))
             .set('trade', path.resolve(__dirname, 'src/components/trade'))
             .set('work', path.resolve(__dirname, 'src/components/work'));
-          config.resolve.alias
+        config.resolve.alias
             .set('__STATIC__', 'static');
 
         config.devServer
@@ -38,9 +36,9 @@ module.exports = {
             .host('localhost')
             .port(8080)
             .hotOnly(true)
-            .watchOptions({ poll: 1000 })
+            .watchOptions({poll: 1000})
             .https(false)
-            .headers({ "Access-Control-Allow-Origin": ["*"] });
+            .headers({"Access-Control-Allow-Origin": ["*"]});
         config.module.rules.delete('eslint');
     },
 }
