900c11dedea95a5f5e748cc234d5540ec0dfdde2
Francisco Kahil <codexico@gmail.com>
2021-07-07 19:04:29 +0300

    trad-33

--- src/components/trade/History.vue
+++ src/components/trade/History.vue
@@ -1,54 +1,49 @@
 <template>
-  <v-card class=" "  >
+  <v-card class="">
     <v-card-title>Charts</v-card-title>
-  <v-tabs
-          v-model="currentTab"
-          centered
-          slider-color="yellow"
-        >
-          <v-tab
-            v-for="i in stocks"
-            :key="i.innerName"
-            :href="`#${i.innerName}`"
-          >
-             {{ i.publicName }}
-          </v-tab>
-        </v-tabs>
-   <v-tabs-items v-model="currentTab">
+    <v-tabs v-model="currentTab" centered slider-color="yellow">
+      <v-tab v-for="i in stocks" :key="i.innerName" :href="`#${i.innerName}`">
+        {{ i.publicName }}
+      </v-tab>
+    </v-tabs>
+    <v-tabs-items v-model="currentTab">
       <v-tab-item
-         eager
+        eager
         v-for="i in stocks"
         :key="i.innerName"
         :value="`${i.innerName}`"
       >
         <v-card flat>
-          <v-card-text  >
-            <chart :stock-name='i.innerName'></chart>
+          <v-card-text>
+            <chart :stock-name="i.innerName"></chart>
           </v-card-text>
         </v-card>
       </v-tab-item>
     </v-tabs-items>
-
   </v-card>
 </template>
 <script>
-import _ from 'lodash'
-import { mapState } from "vuex";
-import Chart from './SingleChart' 
+import _ from "lodash";
+import { mapState, mapActions } from "vuex";
+import Chart from "./SingleChart";
 export default {
-  components:{Chart},
+  components: { Chart },
   data: () => ({
-    chartStocks:[],
-    currentTab:'a',
-
+    chartStocks: [],
+    currentTab: "a",
   }),
-  computed:{
-    ...mapState(['stocks']),
-    getCurrentTab(){
-      
-      return _.find(this.stocks, (i)=>(i.innerName===this.currentTab))
-      }
+  watch: {
+    currentTab(newTab, oldTab) {
+      this.sendEventToServer({
+        name: "changeChartTab",
+        newChartTab: newTab,
+        oldChartTab: oldTab,
+      });
+    },
   },
-  
+  computed: {
+    ...mapState(["stocks"]),
+  },
+  methods: { ...mapActions(["sendEventToServer"]) },
 };
 </script>
